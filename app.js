c56a384792a1ba2455f03ff136c13632b683724a
Jeremiah Kendl <kendljeremiah@gmail.com>
2022-09-26 22:21:42 -0500

    app file
    added function names and inquirer priompts

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
-const express = require('express');
+const inquirer = require('inquirer');
+const fs = require("fs");
 const mysql = require('mysql2');
+const { createConnection } = require('net');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// create the connection to database
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'employeeDb',
+    password: 'password'
+})
 
-app.use
+const promptMenu = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'exit']
+        }])
+        .then(userChoice => {
+            switch (userChoice.menu) {
+                case 'view all departments':
+                    viewAllDepartments();
+                    break;
+                case 'view all roles':
+                    viewAllRoles();
+                    break;
+                case 'view all employees':
+                    viewAllEmployees();
+                    break;
+                case 'add a department':
+                    addDepartment();
+                    break;
+                case 'add a role':
+                    addRole();
+                    break;
+                case 'add an employee':
+                    addEmployee();
+                    break;
+                case 'update an employee role':
+                    updateRole();
+                    break;
+                default:
+                    process.exit();
+            }
+        });
+};
+
+const viewAllDepartments = () => {
+
+};
+
+const viewAllRoles = () => {
+
+};
+
+const selectEmployees
